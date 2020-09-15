24713234afa9ea7f7e5bcaaf27427dd036026343
Julia Pak <41366455+iNeedHelpX@users.noreply.github.com>
2020-09-14 23:35:55 -0400

    Add files via upload

--- main.dart
+++ main.dart
@@ -72,12 +72,6 @@ class LoginPage extends StatelessWidget {
     return null;
   }
 
-  // Future<int> attemptSignUp(String username, String password) async {
-  //   var res = await http.post('$SERVER_IP/signup',
-  //       body: {"username": username, "password": password});
-  //   return res.statusCode;
-  // }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -115,34 +109,6 @@ class LoginPage extends StatelessWidget {
                     }
                   },
                   child: Text("Log In")),
-              // FlatButton(
-              //     onPressed: () async {
-              //       var username = _usernameController.text;
-              //       var password = _passwordController.text;
-
-              //       if (username.length < 4)
-              //         displayDialog(context, "Invalid Username",
-              //             "The username should be at least 4 characters long");
-              //       else if (password.length < 4)
-              //         displayDialog(context, "Invalid Password",
-              //             "The password should be at least 4 characters long");
-              //       else {
-              //         var res = await attemptSignUp(username, password);
-              //         if (res == 201)
-              //           displayDialog(context, "Success",
-              //               "The user was created. Log in now.");
-              //         else if (res == 409)
-              //           displayDialog(
-              //               context,
-              //               "That username is already registered",
-              //               "Please try to sign up using another username or log in if you already have an account.");
-              //         else {
-              //           displayDialog(
-              //               context, "Error", "An unknown error occurred.");
-              //         }
-              //       }
-              //     },
-              //     child: Text("Sign Up"))
             ],
           ),
         ));
